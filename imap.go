8c2847f10045487d5db5885708f335277b42e7e3
linyows <linyows@gmail.com>
2020-06-16 17:50:16 +0900

    add imap client

--- imap.go
+++ imap.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,25 +16,27 @@ type Cmd struct {
 }
 
 type IMAP struct {
-	host  string
-	port  int
+	addr  string
 	count int
 	conn  net.Conn
 }
 
-func NewIMAP(h string, p int) (*IMAP, error) {
-	addr := fmt.Sprintf("%s:%d", h, p)
+func NewIMAP(addr string) (*IMAP, error) {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
 	conn, err := net.DialTimeout("tcp", addr, 30*time.Second)
 	if err != nil {
 		return nil, err
 	}
 
-	if p == 993 {
-		t := tls.Client(conn, &tls.Config{ServerName: h})
-		return &IMAP{host: h, port: p, count: 0, conn: t}, nil
+	if port == "993" {
+		t := tls.Client(conn, &tls.Config{ServerName: host})
+		return &IMAP{addr: addr, count: 0, conn: t}, nil
 	}
 
-	return &IMAP{host: h, port: p, count: 0, conn: conn}, nil
+	return &IMAP{addr: addr, count: 0, conn: conn}, nil
 }
 
 func (i *IMAP) Cmd(name string, args ...string) (*Cmd, error) {
@@ -47,8 +49,11 @@ func (i *IMAP) Cmd(name string, args ...string) (*Cmd, error) {
 	reader := bufio.NewReader(i.conn)
 	for {
 		msg, err := reader.ReadString('\n')
-		if err == io.EOF || err != nil {
+		if err == io.EOF {
 			break
+		}
+		if err != nil {
+			return cmd, err
 		}
 		cmd.res = append(cmd.res, msg)
 		if strings.HasPrefix(msg, tag) {
@@ -66,5 +71,5 @@ var count = 0
 
 func GenTag(k string) string {
 	count += 1
-	return string([]rune(k)[:2]) + string(count)
+	return string([]rune(k)[:2]) + strconv.Itoa(count)
 }
